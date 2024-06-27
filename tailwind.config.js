81722dc116c9b1491a706778ba5c1dbf0c845ced
murtaja <albasti1100@gmail.com>
2024-06-27 13:03:24 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  prefix: "tw-",
+  important: true,
+    corePlugins: {
+        preflight: false,
+    },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
